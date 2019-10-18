f4a32bbb32832044f946a6585d4cdf10b6178f59
James <kingdon909@gmail.com>
2019-10-18 02:05:01 -0400

    retrieve only food & drink events for today

--- index.js
+++ index.js
@@ -12,20 +12,51 @@ fs.readFile('./charlotte-events/private-token', 'utf8', (err, data) => {
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/events', (req, res) => {
-    let options = {
-        url: 'https://www.eventbriteapi.com/v3/events/search?start_date.keyword=today&location.address=charlotte&location.within=10km',
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
-    };
+app.get('/events', async (req, res) => {
+    let response = {
+        events: null
+    }
+    let allEventUrls = []
+    let totalPages = 1
+    let i = 1
     let eventResponse;
-    request(options, function(error, response, body) {
-        console.log('STATUS: ' + response.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(response.headers));
-        eventResponse = body
-        res.send(eventResponse)
-    })
+    do {
+        eventResponse = await getEvents(i)
+        eventResponse = JSON.parse(eventResponse)
+        totalPages = eventResponse.pagination.page_count
+        let eventUrls = getUrls(eventResponse.events)
+        allEventUrls.push(...eventUrls)
+        i++
+    }
+    while (i <= totalPages)
+    response.events = allEventUrls
+    res.send(response)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+let getEvents = async (page) => {
+    let categories = [110]
+    let options = {
+        url: `https://www.eventbriteapi.com/v3/events/search?start_date.keyword=today&location.address=charlotte&location.within=10km&categories=${categories.join()}&page=${page}`,
+        headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json'
+        },
+    };
+    return new Promise((resolve, reject) => {
+        request(options, function(error, response, body) {
+            console.log('STATUS: ' + response.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(response.headers))
+            resolve(body)
+        })
+    })
+}
+
+let getUrls = (events) => {
+    let urls = []
+    events.forEach((event) => {
+        urls.push(event.url)
+    })
+    return urls
+}
